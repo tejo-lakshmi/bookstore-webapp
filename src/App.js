4946150f87294af21c81193997a6021a440e9288
LawJarp-A <prajwalanagani@gmail.com>
2020-12-04 12:58:48 +0530

    Added components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import HomePage from "./components/Home/HomePage";
+import Header from "./components/LogoHeading/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Router>
+        <Switch>
+          <Route path="/register"></Route>
+          <Route path="/login"></Route>
+          <Route path="/home">
+            <HomePage></HomePage>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Posts/Post.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Post(props) {
+    return (
+      <div className="Post">
+        <div className='info'>
+            <p>{props.post.name}</p>
+            <p>{props.post.years_used}</p>
+            <p>{props.post.subject}</p>
+        </div>
+        <div className='price'>
+            <p>{props.post.price}</p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Post;
