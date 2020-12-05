346a78765b4bed64dc1beeb782bafc6ef0e59be8
LawJarp-A <prajwalanagani@gmail.com>
2020-12-06 01:08:26 +0530

    Updated to use react hooks and axios for api requests

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import HomePage from "./components/Home/HomePage";
 import Header from "./components/LogoHeading/Header";
+import LoginPage from "./components/LoginPage/LoginPage"
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/register"></Route>
-          <Route path="/login"></Route>
+          <Route path="/login"><LoginPage></LoginPage></Route>
           <Route path="/home">
             <HomePage></HomePage>
           </Route>
--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,3 +1,25 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from "react";
 
+const axios = require("axios");
+
+function LoginPage() {
+  function login() {
+    axios
+      .post("http://localhost:5001/api/login", {
+        email: "prajwalanagani@gmail.com",
+        password: "pass",
+      })
+      .then((res) => {
+        console.log(res.data);
+        localStorage.setItem('accessToken', res.data.accessToken);
+      });
+  }
+  return (
+    <div className="LoginPage">
+      <h1>Work in progress</h1>
+      <button onClick={login}>LOGIN</button>
+    </div>
+  );
+}
+
+export default LoginPage;
